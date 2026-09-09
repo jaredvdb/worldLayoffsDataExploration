ad42a38119637bc4b694970486362b61f43b0afa
jaredvdb <jared.vandenbosch@gmail.com>
2026-09-09 12:13:19 -0400

    Enhance duplicate identification and removal in SQL
    Added duplicate counting and removal logic to the SQL script.

--- 02-IdentifyingDuplicates.sql
+++ 02-IdentifyingDuplicates.sql
@@ -18,6 +18,31 @@ SELECT *,
 ROW_NUMBER() OVER(PARTITION BY company, location, industry, total_laid_off, percentage_laid_off, date, stage, country, funds_raised_millions) AS dupl_count
 FROM layoffs_test
 WHERE company = 'Casper' OR company = 'Cazoo' OR company = 'Hibob' OR company = 'Wildlife Studios' OR company = 'Yahoo'
-ORDER BY company
+ORDER BY company;
 
--- I was able to see some, i.e. Casper had 3 entries with the last 2 entries being complete duplicates. I'll move to keep one of each duplicate instance 
+-- I was able to see some, i.e. Casper had 3 entries with the last 2 entries being complete duplicates. I'll move to keep one of each duplicate instance, by first officially adding in the column
+ALTER TABLE layoffs_test ADD COLUMN dupl_count INT;
+
+-- Utilize the hidden ctid as the unique identifier to update the new dupl_count
+WITH duplicate_layoffs AS (
+    SELECT 
+        ctid,
+        ROW_NUMBER() OVER (
+            PARTITION BY company, location, industry, total_laid_off, percentage_laid_off, date, stage, country, funds_raised_millions
+            ORDER BY ctid
+        ) AS dupl_count
+    FROM layoffs_test
+)
+UPDATE layoffs_test
+SET dupl_count = duplicate_layoffs.dupl_count
+FROM duplicate_layoffs
+WHERE layoffs_test.ctid = duplicate_layoffs.ctid;
+
+-- I can now see the duplicate values easier, I'll move to drop them (reusing the first query here after the second shows no more values of 2, confirming the deletion)
+SELECT * 
+FROM layoffs_test
+WHERE dupl_count > 1;
+
+DELETE
+FROM layoffs_test
+WHERE dupl_count > 1;
